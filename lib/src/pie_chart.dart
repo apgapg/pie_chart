ce8e3e4a6fc4d09f9a0a4e5346b661e5d5df99a2
Sahil Kumar <xdsahil@gmail.com>
2019-11-09 05:19:08 +0530

    Add enum LegendPosition to align chart legend
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- lib/src/pie_chart.dart
+++ lib/src/pie_chart.dart
@@ -4,11 +4,14 @@ import 'chart_painter.dart';
 import 'legend.dart';
 import 'utils.dart';
 
+enum LegendPosition { top, bottom, left, right }
+
 class PieChart extends StatefulWidget {
   PieChart({
     @required this.dataMap,
     this.showChartValueLabel = false,
     this.chartValueStyle = defaultChartValueStyle,
+    this.legendPosition = LegendPosition.right,
     this.chartValueBackgroundColor = Colors.grey,
     this.legendStyle = defaultLegendStyle,
     this.chartRadius,
@@ -33,6 +36,8 @@ class PieChart extends StatefulWidget {
 
   //Legend styling
   final TextStyle legendStyle;
+
+  final LegendPosition legendPosition;
 
   final double chartRadius;
   final Duration animationDuration;
@@ -121,62 +126,119 @@ class _PieChartState extends State<PieChart>
     super.dispose();
   }
 
+  _getChart() {
+    return Flexible(
+      child: LayoutBuilder(
+        builder: (_, c) => Container(
+          height: widget.chartRadius != null
+              ? c.maxWidth < widget.chartRadius
+                  ? c.maxWidth
+                  : widget.chartRadius
+              : null,
+          child: CustomPaint(
+            painter: PieChartPainter(
+              _fraction,
+              widget.showChartValuesOutside,
+              widget.colorList,
+              chartValueStyle: widget.chartValueStyle,
+              chartValueBackgroundColor: widget.chartValueBackgroundColor,
+              values: legendValues,
+              initialAngle: widget.initialAngle,
+              showValuesInPercentage: widget.showChartValuesInPercentage,
+              decimalPlaces: widget.decimalPlaces,
+              showChartValueLabel: widget.showChartValueLabel,
+            ),
+            child: AspectRatio(aspectRatio: 1),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _getLegend(EdgeInsets legendSpacing) {
+    if (widget.showLegends) {
+      return Flexible(
+        fit: FlexFit.loose,
+        child: Padding(
+          padding: legendSpacing,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: legendTitles
+                .map((item) => Legend(
+                      title: item,
+                      color: getColor(
+                        widget.colorList,
+                        legendTitles.indexOf(item),
+                      ),
+                      style: widget.legendStyle,
+                    ))
+                .toList(),
+          ),
+        ),
+      );
+    }
+  }
+
+  _getPieChart() {
+    switch (widget.legendPosition) {
+      case LegendPosition.top:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _getLegend(
+              EdgeInsets.only(
+                bottom: widget.chartLegendSpacing,
+              ),
+            ),
+            _getChart(),
+          ],
+        );
+      case LegendPosition.bottom:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _getChart(),
+            _getLegend(
+              EdgeInsets.only(
+                top: widget.chartLegendSpacing,
+              ),
+            ),
+          ],
+        );
+      case LegendPosition.left:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _getLegend(
+              EdgeInsets.only(
+                right: widget.chartLegendSpacing,
+              ),
+            ),
+            _getChart(),
+          ],
+        );
+      case LegendPosition.right:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _getChart(),
+            _getLegend(
+              EdgeInsets.only(
+                left: widget.chartLegendSpacing,
+              ),
+            ),
+          ],
+        );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
       padding: EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Flexible(
-            child: LayoutBuilder(
-              builder: (_, c) => Container(
-                height: widget.chartRadius != null
-                    ? c.maxWidth < widget.chartRadius
-                        ? c.maxWidth
-                        : widget.chartRadius
-                    : null,
-                child: CustomPaint(
-                  painter: PieChartPainter(
-                    _fraction,
-                    widget.showChartValuesOutside,
-                    widget.colorList,
-                    chartValueStyle: widget.chartValueStyle,
-                    chartValueBackgroundColor: widget.chartValueBackgroundColor,
-                    values: legendValues,
-                    initialAngle: widget.initialAngle,
-                    showValuesInPercentage: widget.showChartValuesInPercentage,
-                    decimalPlaces: widget.decimalPlaces,
-                    showChartValueLabel: widget.showChartValueLabel,
-                  ),
-                  child: AspectRatio(aspectRatio: 1),
-                ),
-              ),
-            ),
-          ),
-          if (widget.showLegends) ...[
-            SizedBox(width: widget.chartLegendSpacing ?? 16.0),
-            Flexible(
-              fit: FlexFit.loose,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: legendTitles
-                    .map((item) => Legend(
-                          title: item,
-                          color: getColor(
-                            widget.colorList,
-                            legendTitles.indexOf(item),
-                          ),
-                          style: widget.legendStyle,
-                        ))
-                    .toList(),
-              ),
-            ),
-          ]
-        ],
-      ),
+      child: _getPieChart(),
     );
   }
 }
