0d9fc8efe4bbc4dea3cae7d8ec84911d5125bbaa
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-04-09 05:03:31 +0000

    README.md has been updated with issues

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ayushpgupta/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ayushpgupta/StudioProjects/pie_chart/example"
+export "FLUTTER_ROOT=/opt/hostedtoolcache/flutter/1.16.3-dev/x64"
+export "FLUTTER_APPLICATION_PATH=/home/runner/work/pie_chart/pie_chart/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ayushpgupta/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/opt/hostedtoolcache/flutter/1.16.3-dev/x64/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
