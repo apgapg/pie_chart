0a3166a34bd1c63579fa8c68fd73d3425639efcf
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-09-05 10:21:27 +0530

    Update example code

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ayushpgupta/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ayushpgupta/StudioProjects/pie_chart/example"
-export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ayushpgupta/development/flutter/bin/cache/artifacts/engine/darwin-x64"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
