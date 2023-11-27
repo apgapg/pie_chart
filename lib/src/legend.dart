10a1d37f63c648c037405db699d539228d2f5efa
tajjacob <63710008+tajjacob@users.noreply.github.com>
2023-11-27 15:58:48 +0800

    💄 ui: fix legend label text lines in Legend

--- lib/src/legend.dart
+++ lib/src/legend.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../pie_chart.dart';
+
 class Legend extends StatelessWidget {
   const Legend({
     required this.title,
     required this.color,
     required this.style,
     required this.legendShape,
+    required this.position,
     Key? key,
   }) : super(key: key);
 
@@ -13,37 +16,45 @@ class Legend extends StatelessWidget {
   final Color color;
   final TextStyle style;
   final BoxShape legendShape;
+  final LegendPosition position;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 2.0),
-          height: 20.0,
-          width: 18.0,
-          decoration: BoxDecoration(
-            shape: legendShape,
-            color: color,
+    double sw = MediaQuery.of(context).size.width;
+    return SizedBox(
+      width:
+          (position == LegendPosition.right || position == LegendPosition.left)
+              ? sw * 0.5
+              : sw,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 2.0),
+            height: 20.0,
+            width: 18.0,
+            decoration: BoxDecoration(
+              shape: legendShape,
+              color: color,
+            ),
           ),
-        ),
-        const SizedBox(
-          width: 8.0,
-        ),
-        Flexible(
-          fit: FlexFit.loose,
-          child: Text(
-            title,
-            style: style,
-            softWrap: true,
+          const SizedBox(
+            width: 8.0,
           ),
-        ),
-        const SizedBox(
-          width: 8.0,
-        ),
-      ],
+          Flexible(
+            fit: FlexFit.loose,
+            child: Text(
+              title,
+              style: style,
+              softWrap: true,
+            ),
+          ),
+          const SizedBox(
+            width: 8.0,
+          ),
+        ],
+      ),
     );
   }
 }
